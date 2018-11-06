0fdbd450d78c055767bb9e05dd8200e538873900
Tonis Tiigi <tonistiigi@gmail.com>
2018-11-06 15:08:10 -0800

    commands: allow auth from docker config
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- cmd/ctr/commands/resolver.go
+++ cmd/ctr/commands/resolver.go
@@ -18,9 +18,12 @@ package commands
 
 import (
 	"bufio"
+	"bytes"
 	gocontext "context"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -56,18 +59,45 @@ func passwordPrompt() (string, error) {
 	return string(line), nil
 }
 
+type authConfig struct {
+	Auths map[string]authRecord `json:"auths"`
+}
+
+type authRecord struct {
+	Auth string `json:"auth"`
+}
+
 // GetResolver prepares the resolver from the environment and options
 func GetResolver(ctx gocontext.Context, clicontext *cli.Context) (remotes.Resolver, error) {
 	username := clicontext.String("user")
 	var secret string
-	if i := strings.IndexByte(username, ':'); i > 0 {
+	if i := strings.IndexByte(username, ':'); !strings.HasPrefix(username, "file:") && i > 0 {
 		secret = username[i+1:]
 		username = username[0:i]
 	}
 	options := docker.ResolverOptions{
 		Tracker: PushTracker,
 	}
-	if username != "" {
+	var cfg authConfig
+	if strings.HasPrefix(username, "file:") {
+		username = strings.TrimPrefix(username, "file:")
+		dt, err := ioutil.ReadFile(username)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := json.Unmarshal(dt, &cfg); err != nil {
+			return nil, err
+		}
+
+		if len(cfg.Auths) == 0 {
+			cfg.Auths = map[string]authRecord{}
+			if err := json.Unmarshal(dt, &cfg.Auths); err != nil {
+				return nil, err
+			}
+		}
+		
+	} else if username != "" {
 		if secret == "" {
 			fmt.Printf("Password: ")
 
@@ -85,7 +115,22 @@ func GetResolver(ctx gocontext.Context, clicontext *cli.Context) (remotes.Resolv
 
 	hostOptions := config.HostOptions{}
 	hostOptions.Credentials = func(host string) (string, string, error) {
-		// If host doesn't match...
+		if host == "registry-1.docker.io" {
+			host = "https://index.docker.io/v1/"
+		}
+		ac, ok := cfg.Auths[host]
+		if ok {
+			dt, err := base64.StdEncoding.DecodeString(ac.Auth)
+			if err != nil {
+				return "", "", err
+			}
+			parts := bytes.SplitN(dt, []byte(":"), 2)
+			if len(parts) != 2 {
+				return "", "", errors.Errorf("invalid auth config")
+			}
+			username = string(parts[0])
+			secret = string(parts[1])
+		}
 		// Only one host
 		return username, secret, nil
 	}
